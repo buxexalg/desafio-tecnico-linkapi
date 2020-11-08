3613ad67628b33bd0b238fea17846028aa66d170
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-11-08 20:47:37 -0300

    Documentação: Adicionadas rotas de upload e get para o banco de dados.

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const Router = require('koa-router');
 const Pipedrive = require('./controllers/pipedrive');
 const Bling = require('./controllers/bling');
+const DBOperations = require('./db/dbOperations');
+
 const { checkTokenExpiry } = require('pipedrive/lib/OAuthManager');
 
 const router = new Router();
@@ -10,5 +12,8 @@ router.get('/deals', Pipedrive.getWonDealsWithProducts);
 router.get('/orders', Bling.getOrders);
 
 router.post('/export', Bling.exportOrder);
+
+router.post('/upload', DBOperations.dbUpload);
+router.get('/db', DBOperations.dbGet);
 
 module.exports = router;
