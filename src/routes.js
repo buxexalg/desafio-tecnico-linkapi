15bb7a65bbc529a3d52dd90282e38d1141d514f6
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-11-09 00:03:40 -0300

    Funcionalidade: Adicionados comentários nas funções.

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,33 @@
 const Router = require('koa-router');
+
 const Pipedrive = require('./controllers/pipedrive');
 const Bling = require('./controllers/bling');
 const DBOperations = require('./db/dbOperations');
 
-const { checkTokenExpiry } = require('pipedrive/lib/OAuthManager');
-
 const router = new Router();
-
+/***
+ * Endpoint que retorna todas as oportunidades ganhas com os produtos associadas a elas do Pipedrive.
+ */
 router.get('/deals', Pipedrive.getWonDealsWithProducts);
 
+/**
+ * Endpoint que retorna todos os pedidos registrados no Bling.
+ */
 router.get('/orders', Bling.getOrders);
 
+/**
+ * Endpoint que exporta para o Bling as oportunidades ganhas do pipedrive em conjunto com os seus produtos.
+ */
 router.post('/export', Bling.exportOrder);
 
+/**
+ * Endpoint que faz o upload para o MongoDB com todos os pedidos do Bling.
+ */
 router.post('/upload', DBOperations.dbUpload);
+
+/**
+ * Endpoint que retorna os documentos do MongoDB.
+ */
 router.get('/db', DBOperations.dbGet);
 
 module.exports = router;
